2c717b3be423765705646adbe5f8f568d92bee72
Łukasz Miklas <lukasz@revdebug.com>
2023-01-10 15:25:57 +0100

    Add endpoint with RevDeBug version

---
+++ dotNetEndpoint/Controllers/GetSettingsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dotNetEndpoint.Controllers
+{
+    [Route("[controller]")]
+    public class GetSettingsController : Controller
+    {
+        [Route("version")]
+        public string Version()
+        {
+            using (System.IO.StreamReader r = new System.IO.StreamReader("dotNetEndpoint.deps.json"))
+            {
+                string json = r.ReadToEnd();
+                return json;
+            }
+        }
+    }
+}
